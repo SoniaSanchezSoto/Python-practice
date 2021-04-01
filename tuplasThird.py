badac20c0f425251dd3b8d75aaff1b14f93328fb
sonya <sonyasanchezsoto@gmail.com>
2021-03-31 20:04:59 -0500

    tuplas

---
+++ tuplasThird.py
@@ -0,0 +1,32 @@
+#mitupla=("Juan",13,1,1995)
+#print(mitupla[3])
+
+#mitupla=("Juan",13,1,1995)
+#milista=list(mitupla)
+#print(milista)
+
+#mitupla=("Juan",13,1,1995)
+#milista=list(mitupla)
+#print(mitupla)
+
+#milista=["Juan", 13,1,1995]
+#mitupla=tuple(milista)
+#print("Juan" in mitupla)
+
+#milista=["Juan", 13,1,1995,13]
+#mitupla=tuple(milista)
+#print(mitupla.count(13))
+
+#milista=["Juan", 13,1,1995,13]
+#mitupla=tuple(milista)
+#print(len(mitupla))
+
+#mitupla="Juan", 13,1,1995
+#print(mitupla)
+
+mitupla=("Juan", 13,1,1995)
+nombre, dia, mes, agno=mitupla
+print(nombre)
+print(dia)
+print(agno)
+print(mes)
