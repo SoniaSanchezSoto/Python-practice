fcaf46e86ae997bb4c7240f9fd868a371fa9d5fd
sonya <sonyasanchezsoto@gmail.com>
2021-04-01 20:38:44 -0500

    diccionario

---
+++ diccionarioFourth.py
@@ -0,0 +1,35 @@
+#midiccionario={"Alemania":"Berlín","Francia":"París","Reino Unido":"londres","España":"Madrid"}
+#print(midiccionario["Francia"])
+
+#midiccionario={"Alemania":"Berlín","Francia":"París","Reino Unido":"londres","España":"Madrid"}
+#midiccionario["italia"]="Lisboa"
+#print(midiccionario)
+#midiccionario["Italia"]="Roma"
+#print(midiccionario)
+
+#midiccionario={"Alemania":"Berlín","Francia":"París","Reino Unido":"londres","España":"Madrid"}
+#midiccionario["italia"]="Lisboa"
+#print(midiccionario)
+#midiccionario["Italia"]="Roma"
+#print(midiccionario)
+#del midiccionario["Reino Unido"]
+#print(midiccionario)
+
+#midiccionario={"Alemania":"Berlín",23:"Jordan","Mosqueteros":3}
+#print(midiccionario)
+
+#mitupla=["España","Francia","Reino Unido","Alemania"]
+#midiccionario={mitupla[0]:"Madrid",mitupla[1]:"París",mitupla[2]:"londres",mitupla[3]:"Berlín"}
+#print(midiccionario["Francia"])
+
+#midiccionario={23:"Jordan","Nombre":"Michael","Equipo":"Chicago","anillos":[1991,1992,1993,1996,1997,1998]}
+#print(midiccionario["Equipo"])
+
+#midiccionario={23:"Jordan","Nombre":"Michael","Equipo":"Chicago","anillos":[1991,1992,1993,1996,1997,1998]}
+#print(midiccionario["anillos"])
+
+midiccionario={23:"Jordan","Nombre":"Michael","Equipo":"Chicago","anillos":{"temporadas":[1991,1992,1993,1996,1997,1998]}}
+print(midiccionario.keys())
+print(midiccionario.values())
+print(midiccionario)
+print(len(midiccionario))
