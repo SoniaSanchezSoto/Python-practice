5dd4dd8b0c62ff6c4bf50028f0eea8122d79cdcd
SoniaSanchezSoto <81663053+SoniaSanchezSoto@users.noreply.github.com>
2021-04-11 22:51:16 -0500

    Add files via upload

---
+++ alturaTriangulo.py
@@ -0,0 +1,9 @@
+'''n=int(input("Introduce la altura del triángulo (entero positivo):  "))
+for i in range(n):
+    for j in range(i+1):
+        print("*", end="")
+    print("")'''
+
+n=int(input("Introduce la altura del triángulo (entero positivo):  "))
+for i in range(n):
+    print("*"*(i+1))
