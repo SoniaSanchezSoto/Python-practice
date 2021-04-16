2f2a0c2c5db03b830521c7c4da4bf97d054c5846
SoniaSanchezSoto <81663053+SoniaSanchezSoto@users.noreply.github.com>
2021-04-15 22:57:33 -0500

    Delete dosNumeros.py

--- dosNumeros.py
+++
@@ -1,9 +0,0 @@
-n1=int(input("ingrese el primer número"))
-n2=int(input("ingrese el segundo número"))
-
-if n1>n2:
-    print "el mayor es %d"%n1
-elif n2>n1:
-    print "el mayor es %d"%n2
-else:
-    print "son iguales"
