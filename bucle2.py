0b91eaaedd8303b11a8098528d7244b8aa0da4a1
sonya <sonyasanchezsoto@gmail.com>
2021-04-25 23:42:02 -0500

    Bucles 2

---
+++ bucle2.py
@@ -0,0 +1,36 @@
+'''i=1
+
+while i<=10:
+    print("Ejecución "+str(i))
+    i=i+1
+
+print("Terminó la ejecución")
+
+edad=int(input("Introduce tu edad por favor: "))
+
+while edad<5 or edad>100: 
+    print("Has introducido una edad incorrecta. Vuelve a intentarlo")
+    edad=int(input("Introduce tu edad por favor: "))
+print("Gracias por colaborar. Puedes pasar")
+print("Edad del aspirante"+str(edad))'''
+
+import math
+print("Programa de Cálculo de raíz cuadrada")
+numero=int(input("Introduce un número por favor:  "))
+
+intentos=0
+
+while numero<0:
+    print("No se puede hallar la raíz de un número negativo")
+
+    if intentos==2:
+        print("Has consumido demasiados intentos. El programa ha finalizado")
+        break;
+    
+    numero=int(input("Introduce un número por favor:  "))
+    if numero<0:
+        intentos=intentos+1
+
+if intentos<2:
+    solucion=math.sqrt(numero)
+    print("La raíz cuadrada de "+str(numero)+" es " + str(solucion))
