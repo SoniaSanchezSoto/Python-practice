58c1f96116587add279c4354bca60644e699164d
sonya <sonyasanchezsoto@gmail.com>
2021-05-18 23:22:38 -0500

    Sucesiones

---
+++ sucesiones.py
@@ -0,0 +1,24 @@
+def main():
+    for i in range(10):
+        print(f"{2 * i + 2} ", end="")
+    print()
+
+    for i in range(8):
+        print(f"{2 * i + 20} ", end="")
+    print()
+
+    for i in range(6):
+        print(f"{4 * i + 10} ", end="")
+    print()
+
+    for i in range(9):
+        print(f"{-5 * i + 40} ", end="")
+    print()
+
+    for i in range(10):
+        print(f"{i + 1} ", end="")
+    print()
+
+
+if __name__ == "__main__":
+    main()
