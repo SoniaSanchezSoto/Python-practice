9a9e87db8143bc69431954667d67f427f594b75e
sonya <sonyasanchezsoto@gmail.com>
2021-05-26 23:34:35 -0500

    Tablas de Multiplicar 13, 14

---
+++ tabla_multiplicar13.py
@@ -0,0 +1,18 @@
+"""
+	Imprimir tabla de multiplicar de cualquier número
+	con Python usando el ciclo while
+	@author parzibyte
+"""
+def imprimir_tabla(numero):
+    # Se supone que las tablas llegan hasta el 10
+    LIMITE = 10
+    # Comenzar en 1
+    contador = 1
+    while contador <= LIMITE:
+        resultado = contador * numero
+        print("{} x {} = {}".format(numero, contador, resultado))
+        # Incrementar contador para no caer en ciclo infinito
+        contador = contador + 1
+
+# Probar función
+imprimir_tabla(13)
