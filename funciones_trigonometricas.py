cc66e36e81885dc3b486c0a0112a55ab2104e379
sonya <sonyasanchezsoto@gmail.com>
2021-05-31 23:13:49 -0500

    Funciones Trigonometricas

---
+++ funciones_trigonometricas.py
@@ -0,0 +1,7 @@
+import math
+a=math.sin(1)
+b=math.cos(1)
+c=math.tan(1)
+print(a)	   ##Imprime: 0.841
+print(b)	   ##Imprime: 0.54
+print(c)	   ##Imprime: 1.557
---
+++ radianes.py
@@ -0,0 +1,12 @@
+import math
+
+grados = 60
+radianes = (grados* math.pi)/180
+
+seno = math.sin(radianes)
+coseno = math.cos(radianes)
+tangente = math.tan(radianes)
+
+print(seno)
+print(coseno)
+print(tangente)
