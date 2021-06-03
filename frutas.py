86f272e2afbcfd0b38a6b0a0b6aad5bf1514e383
sonya <sonyasanchezsoto@gmail.com>
2021-06-02 22:31:02 -0500

    Frutas y Lista numeros

---
+++ frutas.py
@@ -0,0 +1,3 @@
+frutas = {'Fresa':'roja', 'Limon':'verde', 'Papaya':'naranja', 'Manzana':'amarilla', 'Guayaba':'rosa'}
+for nombre, color in frutas.items():
+    print (nombre, "es de color", color)
